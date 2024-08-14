94ecfa17a763142eb31ea693214bf9781a26ec66
DaniEC3 <167336437+DaniEC3@users.noreply.github.com>
2024-08-14 17:35:56 -0400

    pokemon displays on the screen
    It adds a "Wow that's big" comment if the pokemon is big.

--- js/scripts.js
+++ js/scripts.js
@@ -14,5 +14,14 @@ let pokemonList = [
 	height : 0.5,
 	types : ['Water']
 	}
+]
 
-	]
+for (let i = 0; i < pokemonList.length; i++){ 
+
+	if (pokemonList[i].height < 0.7){
+		document.write(pokemonList[i].name + ' (height: ' + pokemonList[i].height + 'm). <br/>');
+	}
+	else{
+		document.write(pokemonList[i].name + ' (height: ' + pokemonList[i].height + 'm) ' + '- Wow, that`s big!. <br/>');
+	}
+}
