f66bb79f8d0813a519c427cbab641cc0ae692f96
DaniEC3 <167336437+DaniEC3@users.noreply.github.com>
2024-08-14 11:35:05 -0400

    Pokemon Commit
    Code changed for pokedex achievement.

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,18 @@
-alert('Hello world');
-let favoriteFood = 'Steak';
-document.write(favoriteFood);
+let pokemonList = [
+	{ 
+	name : 'Bulbasaur',
+	height : 0.7, 
+	types : ['Grass','Poison']
+	},
+	{ 
+	name : 'Charmander',
+	height : 0.6,
+	types : ['Fire']
+	},
+	{ 
+	name : 'Squirtle',
+	height : 0.5,
+	types : ['Water']
+	}
+
+	]
